177f6fd46e75ca4f2c4686f9488950b6a71b30a0
JobGetabu <getabujob@gmail.com>
2018-10-26 21:50:10 +0300

    check for login

--- app/src/main/java/com/job/userportal/ui/HomeActivity.java
+++ app/src/main/java/com/job/userportal/ui/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.job.userportal.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -9,26 +12,47 @@ import com.job.userportal.R;
 import com.job.userportal.datasource.rest.ApiUtils;
 import com.job.userportal.datasource.rest.UserApiService;
 import com.job.userportal.model.CreateUser;
+import com.job.userportal.model.Login;
 import com.job.userportal.model.UserResponse;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static com.job.userportal.util.Constants.LOGIN_USER_PREFS;
+
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "Home";
     private UserApiService mUserApiService;
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         mUserApiService = ApiUtils.getUserApiService();
 
         testPostUser();
         testUserFetch();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //checks if a user is currently logged in
+        String user = mSharedPreferences.getString(LOGIN_USER_PREFS,null);
+
+        if (user == null){
+            Intent intent = new Intent(this, Login.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
     private void testUserFetch(){

214f7331b5fd60544e41b973fc50956ac9eeba57
JobGetabu <getabujob@gmail.com>
2018-10-26 21:45:18 +0300

    added register

---
+++ app/src/main/java/com/job/userportal/ui/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.job.userportal.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.job.userportal.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}

6f6eb6abd309736d08d1e89026b83f5f1f97c335
JobGetabu <getabujob@gmail.com>
2018-10-26 21:46:04 +0300

    added constants

---
+++ app/src/main/java/com/job/userportal/util/Constants.java
@@ -0,0 +1,11 @@
+package com.job.userportal.util;
+
+/**
+ * Created by Job on Friday : 10/26/2018.
+ */
+public class Constants {
+
+    //shared prefs
+    public static final String LOGIN_USER_PREFS = "LOGIN_USER_PREFS";
+
+}
